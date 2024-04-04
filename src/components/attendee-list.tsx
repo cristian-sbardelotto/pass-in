f1a0cbbb4af4d0d32d0b07978d280f848136141b
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-04 13:54:41 -0300

    feat: finish home style structure

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,11 @@
-import { SearchIcon } from 'lucide-react';
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronsLeftIcon,
+  ChevronsRightIcon,
+  MoreHorizontalIcon,
+  SearchIcon,
+} from 'lucide-react';
 // 1:08:07
 export function AttendeeList() {
   return (
@@ -6,14 +13,14 @@ export function AttendeeList() {
       <div className='flex items-center justify-between'>
         <h1 className='text-2xl font-bold'>Participantes</h1>
 
-        <div className='px-3 py-1.5 w-72 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3'>
+        <div className='px-3 py-1.5 w-72 border border-white/10 bg-transparent rounded-lg flex items-center gap-3'>
           <SearchIcon
             size={16}
             className='text-emerald-300'
           />
 
           <input
-            className='bg-transparent flex-1 outline-none'
+            className='bg-transparent flex-1 outline-none h-auto text-sm border-0 p-0'
             placeholder='Buscar participante...'
           />
         </div>
@@ -23,61 +30,103 @@ export function AttendeeList() {
         <table className='w-full '>
           <thead>
             <tr className='border-b border-white/10'>
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'>
-                <input type='checkbox' />
+              <th className='w-12 py-3 px-4 text-sm font-semibold text-left'>
+                <input
+                  type='checkbox'
+                  className='size-4 bg-black/20 rounded border border-white/10'
+                />
               </th>
 
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'>
+              <th className='py-3 px-4 text-sm font-semibold text-left'>
                 Código
               </th>
 
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'>
+              <th className='py-3 px-4 text-sm font-semibold text-left'>
                 Participantes
               </th>
 
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'>
+              <th className='py-3 px-4 text-sm font-semibold text-left'>
                 Data de inscrição
               </th>
 
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'>
+              <th className='py-3 px-4 text-sm font-semibold text-left'>
                 Data do check-in
               </th>
 
-              <th className='py-3 px-2.5 text-sm font-semibold text-left'></th>
+              <th className='w-16 py-3 px-4 text-sm font-semibold text-left'></th>
             </tr>
           </thead>
 
           <tbody>
-            <tr className='border-b border-white/10'>
-              <td className='py-3 px-2.5 text-sm text-zinc-300'>
-                <input type='checkbox' />
-              </td>
+            {Array.from({ length: 10 }).map(() => (
+              <tr className='border-b border-white/10 hover:bg-white/5'>
+                <td className='py-3 px-4 text-sm text-zinc-300'>
+                  <input
+                    type='checkbox'
+                    className='size-4 bg-black/20 rounded border border-white/10'
+                  />
+                </td>
 
-              <td className='py-3 px-2.5 text-sm text-zinc-300'>312312</td>
+                <td className='py-3 px-4 text-sm text-zinc-300'>312312</td>
 
-              <td className='py-3 px-2.5 text-sm text-zinc-300'>
-                <div className='flex flex-col gap-1'>
-                  <span className='font-semibold text-white'>
-                    Cristian Sbardelotto
-                  </span>
-                  <span>cristian@gmail.com</span>
-                </div>
-              </td>
+                <td className='py-3 px-4 text-sm text-zinc-300'>
+                  <div className='flex flex-col gap-1'>
+                    <span className='font-semibold text-white'>
+                      Cristian Sbardelotto
+                    </span>
+                    <span>cristian@gmail.com</span>
+                  </div>
+                </td>
 
-              <td className='py-3 px-2.5 text-sm text-zinc-300'>
-                7 dias atrás
-              </td>
-              <td className='py-3 px-2.5 text-sm text-zinc-300'>
-                3 dias atrás
-              </td>
+                <td className='py-3 px-4 text-sm text-zinc-300'>
+                  7 dias atrás
+                </td>
+                <td className='py-3 px-4 text-sm text-zinc-300'>
+                  3 dias atrás
+                </td>
 
-              <td className='py-3 px-2.5 text-sm text-zinc-300'></td>
-            </tr>
+                <td className='py-3 px-4 text-sm text-zinc-300'>
+                  <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                    <MoreHorizontalIcon size={16} />
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
 
           <tfoot>
-            <td colSpan={3}>Mostrando 10 de 228 itens</td>
-            <td colSpan={3}>página 1 de 23</td>
+            <td
+              className='py-3 px-4 text-sm text-zinc-300'
+              colSpan={3}
+            >
+              Mostrando 10 de 228 itens
+            </td>
+            <td
+              className='py-3 px-4 text-sm text-zinc-300 text-right'
+              colSpan={3}
+            >
+              <div className='inline-flex items-center gap-8'>
+                <span>Página 1 de 23</span>
+
+                <div className='flex gap-1.5'>
+                  <button className='bg-white/20 border border-white/10 rounded-md p-1.5'>
+                    <ChevronsLeftIcon size={16} />
+                  </button>
+
+                  <button className='bg-white/20 border border-white/10 rounded-md p-1.5'>
+                    <ChevronLeftIcon size={16} />
+                  </button>
+
+                  <button className='bg-white/20 border border-white/10 rounded-md p-1.5'>
+                    <ChevronRightIcon size={16} />
+                  </button>
+
+                  <button className='bg-white/20 border border-white/10 rounded-md p-1.5'>
+                    <ChevronsRightIcon size={16} />
+                  </button>
+                </div>
+              </div>
+            </td>
           </tfoot>
         </table>
       </div>
